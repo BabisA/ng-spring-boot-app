6af4566e1a971175a5556b772dc91c0de9f0de4c
BabisA <charalampos.amasialidis@onelity.com>
2021-05-05 15:08:49 +0300

    sets up database

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO company (name) VALUES
+  ('CMPN');
+
+INSERT INTO employee (name, surname, email, address, salary, company_id) VALUES
+  ('Christian', 'Brown', 'CBrown@cmpn.com', '2765  Single Street', 3030, 1),
+  ('Bill', 'Nate', 'BNates@cmpn.com', '2599  Gorby Lane', 2650, 1),
+  ('Arianna', 'Smith', 'ASmith@cmpn.com', '2222  My Drive', 2900, 1);
