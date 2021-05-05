d3cc57006b4f4161115e256fbb260f9837100f9f
BabisA <charalampos.amasialidis@onelity.com>
2021-05-05 16:54:05 +0300

    adds DTO classes

---
+++ backend/src/main/java/com/github/babisa/crudapp/dto/EmployeeDto.java
@@ -0,0 +1,69 @@
+package com.github.babisa.crudapp.dto;
+
+import java.math.BigDecimal;
+
+public class EmployeeDto {
+    private Integer id;
+    private Integer companyId;
+    private String name;
+    private String surname;
+    private String email;
+    private String address;
+    private BigDecimal salary;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public BigDecimal getSalary() {
+        return salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+}
