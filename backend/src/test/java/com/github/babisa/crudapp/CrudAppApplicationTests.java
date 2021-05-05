82d915971d4583ac6d92f74536b455b7c9017b2a
BabisA <charalampos.amasialidis@onelity.com>
2021-05-05 10:59:01 +0300

    Initial commit

---
+++ backend/src/test/java/com/github/babisa/crudapp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.babisa.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
