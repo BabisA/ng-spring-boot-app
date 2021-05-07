50539e9ed9ca8c3c9f6cfa2aca060236e56114cd
BabisA <charalampos.amasialidis@onelity.com>
2021-05-07 17:49:45 +0300

    adds company service & model in ng

---
+++ frontend/src/app/service/company.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  private readonly API_URL = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get(`${this.API_URL}/companies`);
+  }
+}
