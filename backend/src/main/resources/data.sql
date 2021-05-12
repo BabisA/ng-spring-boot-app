eb5e2d8471a5d4848990c51388b9b56fe75a874a
BabisA <charalampos.amasialidis@onelity.com>
2021-05-12 09:05:49 +0300

    adds more fake data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,7 +1,12 @@
 INSERT INTO company (name) VALUES
-  ('CMPN');
+  ('Company 1'),
+  ('Company 2');
 
 INSERT INTO employee (name, surname, email, address, salary, company_id) VALUES
-  ('Christian', 'Brown', 'CBrown@cmpn.com', '2765  Single Street', 3030, 1),
-  ('Bill', 'Nate', 'BNates@cmpn.com', '2599  Gorby Lane', 2650, 1),
-  ('Arianna', 'Smith', 'ASmith@cmpn.com', '2222  My Drive', 2900, 1);
+  ('Christian', 'Brown', 'CBrown@test.com', '2765  1790 Elgin St', 3030, 1),
+  ('Bill', 'Nate', 'BNates@test.com', '9796 Plum St', 2650, 1),
+  ('Arianna', 'Smith', 'ASmith@test.com', '2222  My Drive', 2900, 1),
+  ('Vivan', 'Stephens', 'vivan.stephens@test.com', '7136 Pecan Acres Ln', 2900, 1),
+  ('Jacob', 'Json', 'JJson@test.com', '2599  Derby Side', 2650, 2),
+  ('Sue', 'Scott', 'sue.scott@test.com', '1681 E Little York Rd', 3100, 2),
+  ('Wesley', 'Lambert', 'wesley.lambert@test.com', '12719 W Belt Line Rd', 2500, 2);
